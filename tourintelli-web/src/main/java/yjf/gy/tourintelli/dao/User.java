67c9c8dfc18795a7711d627afc0e256fb5dd36e1
YoungJiafang <jiafangbest001@163.com>
2014-09-08 00:53:27 +0800

    add somethings

---
+++ tourintelli-web/src/main/java/yjf/gy/tourintelli/dao/User.java
@@ -0,0 +1,21 @@
+package yjf.gy.tourintelli.dao;
+
+public class User {
+
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
